1ac53467720bca23c768cc2418078e59b0de44fc
mdlinington <mdlinington@gmail.com>
2015-09-12 03:29:41 -0400

    Set up basic UI organization

---
+++ TempoWatch WatchKit Extension/StartMusic.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  TempoWatch WatchKit Extension
+//
+//  Created by Mark Linington on 9/12/15.
+//  Copyright © 2015 MarkLinington. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface StartMusic : WKInterfaceController
+
+@end

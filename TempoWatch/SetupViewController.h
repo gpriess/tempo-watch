1517130717a2124cb24a6244f6d18ab341844b2d
mdlinington <mdlinington@gmail.com>
2015-09-13 04:36:26 -0400

    Updated watch view of album art

---
+++ TempoWatch/SetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetupViewController.h
+//  TempoWatch
+//
+//  Created by Mark Linington on 9/13/15.
+//  Copyright © 2015 MarkLinington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupViewController : UIViewController
+
+@end
